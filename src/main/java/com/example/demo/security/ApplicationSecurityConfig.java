54f6dbd6ea2f2a6a6358db72708125356cca6df8
masprog2022 <masprog2022@gmail.com>
2022-03-21 20:20:24 +0100

    3 - Users Roles and Authorities

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,13 +1,29 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.example.demo.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends SecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
 
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +35,27 @@ public class ApplicationSecurityConfig extends SecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    protected UserDetailsService userDetailsService(){
+
+      UserDetails annaSmith =  User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name())
+                .build();
+
+      UserDetails linda = User.builder()
+              .username("linda")
+              .password(passwordEncoder.encode("password123"))
+              .roles(ADMIN.name())
+              .build();
+
+
+
+        return new InMemoryUserDetailsManager(
+                annaSmith
+        );
     }
 }
